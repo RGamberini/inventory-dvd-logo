ad8723cf67a31f169ef644141bdbc3e0c156cfff
Rudy Gamberini <rudygamberini@gmail.com>
2022-12-02 17:52:39 -0500

    Widget mode

--- src/main/java/com/inventorydvdlogo/InventoryDVDLogoPlugin.java
+++ src/main/java/com/inventorydvdlogo/InventoryDVDLogoPlugin.java
@@ -5,6 +5,8 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.events.BeforeRender;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -22,39 +24,26 @@ import java.util.List;
 public class InventoryDVDLogoPlugin extends Plugin {
     @Inject
     private Client client;
-    @Inject
-    private OverlayManager overlayManager;
-    private Overlay inventory;
     private DVDLogo dvdLogo;
     private final int wallDistance = 1;
 
+
     @Override
     protected void startUp() throws IllegalAccessException {
-        try {
-            Field overlaysField = OverlayManager.class.getDeclaredField("overlays");
-            overlaysField.setAccessible(true);
-
-            inventory = ((List<Overlay>) overlaysField.get(overlayManager)).stream()
-                    .filter(overlay ->
-                            overlay.getName().equals("RESIZABLE_VIEWPORT_BOTTOM_LINE_INVENTORY_PARENT")
-                    )
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("STARTUP FAILED CANNOT FIND INVENTORY"));
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
         dvdLogo = new DVDLogo();
     }
 
     @Subscribe
     public void onBeforeRender(BeforeRender beforeRender) {
+        Widget inventory = client.getWidget(WidgetInfo.TO_GROUP(10747998), 0);
+        if (inventory == null) return;
+        dvdLogo.travel();
+        inventory.setForcedPosition((int) dvdLogo.x, (int) dvdLogo.y);
         Rectangle bounds = inventory.getBounds();
         if (bounds.getMinX() < wallDistance || client.getViewportWidth() - bounds.getMaxX()  < wallDistance)
             dvdLogo.flip(false);
         else if (bounds.getMinY() < wallDistance || client.getViewportHeight() - bounds.getMaxY() < wallDistance)
             dvdLogo.flip(true);
-        dvdLogo.travel();
-        inventory.setPreferredLocation(new Point((int) dvdLogo.x, (int) dvdLogo.y));
-    }
 
+    }
 }
